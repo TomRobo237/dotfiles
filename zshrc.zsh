904db02a0d2f2bcc26aab6e390446ee9590f9e78
Thomas Miller <millertime1989@live.com>
2020-11-21 11:00:31 -0500

    fixing some things

--- zshrc.zsh
+++ zshrc.zsh
@@ -26,6 +26,7 @@ DISABLE_UPDATE_PROMPT="true"
 plugins=(
 	git
   zsh-autosuggestions
+  zsh-syntax-hightlighting
 )
 
 source $ZSH/oh-my-zsh.sh
@@ -60,20 +61,19 @@ alias cdgr='cd $(git rev-parse --show-toplevel); pwd'
 alias cleanup_git_branches='git branch --merged | grep -Ev "(^\*|master|dev)" | xargs git branch -d'
 
 # OSX Section
-        # adding nix flag to ignore symlink warnings since firmlinks are a thing in OSX catalina...
-        export NIX_IGNORE_SYMLINK_STORE=1
-        source /Users/tmiller/.nix-profile/etc/profile.d/nix.sh
-        export NIX_PATH='darwin-config=/Users/tmiller/.nixpkgs/darwin-configuration.nix:/Users/tmiller/.nix-defexpr/channels:/Users/tmiller/.nix-defexpr/channels'
-
-        # Adding Shell integration for ZSH
-        source ~/.iterm2_shell_integration.zsh
-
-        # If you come from bash you might have to change your $PATH.
-        # Make sure .nix_profile goes in the front to ensure that its bins get chosen first
-        PATH="${HOME}/.nix-profile/bin:$(python3 -m site --user-base)/bin:${PATH}"
-        PATH="${PATH}:${HOME}/ubin:${HOME}/ubin/sicom_tools"
-        PATH="${PATH}:${HOME}/ubin/pyscripts:${HOME}/.local/bin"
-
+#         # adding nix flag to ignore symlink warnings since firmlinks are a thing in OSX catalina...
+#         export NIX_IGNORE_SYMLINK_STORE=1
+#         source /Users/tmiller/.nix-profile/etc/profile.d/nix.sh
+#         export NIX_PATH='darwin-config=/Users/tmiller/.nixpkgs/darwin-configuration.nix:/Users/tmiller/.nix-defexpr/channels:/Users/tmiller/.nix-defexpr/channels'
+# 
+#         # Adding Shell integration for ZSH
+#         source ~/.iterm2_shell_integration.zsh
+# 
+#         # If you come from bash you might have to change your $PATH.
+#         # Make sure .nix_profile goes in the front to ensure that its bins get chosen first
+#         PATH="${HOME}/.nix-profile/bin:$(python3 -m site --user-base)/bin:${PATH}"
+#         PATH="${PATH}:${HOME}/ubin/pyscripts:${HOME}/.local/bin"
+# 
 # END OSX Section
 
 # Fuzzy finder
@@ -88,11 +88,3 @@ if test -x "$(command -v fortune)" ; then
 	fi
 fi
 
-# Added by Haskell compiler
-[ -f "${GHCUP_INSTALL_BASE_PREFIX:=$HOME}/.ghcup/env" ] && source "${GHCUP_INSTALL_BASE_PREFIX:=$HOME}/.ghcup/env"
-
-eval "$(direnv hook zsh)"
-
-# Source ADT things
-source ~/.adtzsh
-source /Users/tmiller/git/personal/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
